1c660207cf1bce98b68f9bc99f78ea1010b3e360
Frank Bergmann <frank.bergmann@project-open.com>
2006-04-04 16:32:34 +0000

    - added the update stuff for invoices

--- sql/postgresql/intranet-search-pg-create.sql
+++ sql/postgresql/intranet-search-pg-create.sql
@@ -516,6 +516,48 @@ EXECUTE PROCEDURE im_forum_topics_tsearch();
 
 
 
+
+-----------------------------------------------------------
+-- invoices
+
+-- We are going for Invoice instead of im_costs, because of
+-- performance reasons. There many be many cost items, but
+-- they don't usually interest us very much.
+
+insert into im_search_object_types values (4,'im_invoice');
+
+create or replace function im_invoice_tsearch ()
+returns trigger as '
+declare
+	v_string	varchar;
+begin
+	select  coalesce(i.invoice_nr, '''') || '' '' ||
+		coalesce(c.cost_nr, '''') || '' '' ||
+		coalesce(c.cost_name, '''') || '' '' ||
+		coalesce(c.description, '''') || '' '' ||
+		coalesce(c.note, '''')
+	into
+		v_string
+	from
+		im_invoices i,
+		im_costs c
+	where	
+		i.invoice_id = c.cost_id
+		and i.invoice_id = new.invoice_id;
+
+	perform im_search_update(new.invoice_id, ''im_invoice'', new.invoice_id, v_string);
+	return new;
+end;' language 'plpgsql';
+
+CREATE TRIGGER im_invoices_tsearch_tr
+BEFORE INSERT or UPDATE
+ON im_invoices
+FOR EACH ROW
+EXECUTE PROCEDURE im_invoice_tsearch();
+
+
+
+
 -----------------------------------------------------------
 -- Business Object View URLs
 
@@ -547,4 +589,7 @@ set company_type_id = company_type_id;
 
 update im_forum_topics
 set scope = scope;
+
+update im_invoices
+set invoice_nr = invoice_nr;
 
